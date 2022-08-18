16aa85b5d6202a9485f3fe9c2f7f0b1f6906c7c3
Miguelduran1 <111394881+Miguelduran1@users.noreply.github.com>
2022-08-18 14:59:17 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -41,7 +41,8 @@ drawCircle(width/5, height/2, 20, 'blue');
 
 drawRect(50, 50, 60, 40, 'black');
 
-drawFilledCircle(width - 100, height - 100, 100, 'black');
+drawFilledCircle(width - 100, height - 100,200
+, 'black');
 
 drawFilledRect(5, height - 20, 45, 15, 'purple');
 
